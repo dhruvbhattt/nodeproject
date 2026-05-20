8d28404e019bf3574336da9993e3592e01dc6471
Dhruv Bhatt <dhruvbhatt@microsoft.com>
2026-05-20 10:58:51 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const axios = require('axios');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+//USE YOUR REAL API URL HERE
+const API_URL = "https://DhruvsWebApp2.azurewebsites.net/api/test";
+
+app.get('/', async (req, res) => {
+    try {
+        const response = await axios.get(API_URL);
+
+        if (response.status === 200) {
+            res.send(`Success! ${response.data.message}`);
+        } else {
+            res.send("Failed to call API");
+        }
+    } catch (error) {
+        console.error(error.message);
+        res.send("Error calling API");
+    }
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
